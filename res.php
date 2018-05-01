5de55c3ef00879a431cb3652e0d59ee5b81fa77c
meharshkumar <38858662+meharshkumar@users.noreply.github.com>
2018-05-01 13:51:41 +0530

    Update res.php

--- res.php
+++ res.php
@@ -1,7 +1,7 @@
 <?php
-$mobile="8959177684";
+$mobile="9060295290";
 $message="test message";
-$json = json_decode(file_get_contents("https://smsapi.engineeringtgr.com/send/?Mobile=xxxxxxxxxx&Password=xxxxxx&Message=".urlencode($message)."&To=".urlencode($mobile)."&Key=xxxxxxxxxxxxxx"),true);
+$json = json_decode(file_get_contents("https://smsapi.engineeringtgr.com/send/?Mobile=9060295290&Password=T8924E&Message=".urlencode($message)."&To=".urlencode($mobile)."&Key=xxxxxxxxxxxxxx"),true);
 if ($json["status"]==="success") {
     echo $json["msg"];
     //your code when send success
